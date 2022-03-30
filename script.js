93e657dfb729b0cf8d3826fe455387a8e9d4d928
Demir <dmrarikan@gmail.com>
2022-03-30 17:05:08 +0200

    changed the calculator script to make it more realistic

--- script.js
+++ script.js
@@ -36,39 +36,53 @@ const digits = document.querySelectorAll('.digit')
 const operators = document.querySelectorAll('.operator')
 const equalBtn = document.querySelector('#equal')
 const clearBtn = document.querySelector('.clear')
-let num1, num2, oper
+
+const calc = {
+    displayNum: '',
+    firstNum: null,
+    secNum: null,
+    waitingForSecNum: false,
+    operator: null,
+}
 
 digits.forEach((digit) => {
     digit.addEventListener('click', () => {
-        display.textContent += digit.textContent
+        if(calc.waitingForSecNum === false){
+            calc.displayNum += digit.textContent
+            calc.firstNum = calc.displayNum
+            display.textContent = calc.displayNum 
+        } else {
+            calc.secNum = calc.firstNum
+            calc.displayNum = digit.textContent
+            display.textContent = calc.displayNum
+            calc.waitingForSecNum = false
+        }
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        oper = operator.textContent
-        if(num1 === undefined){
-            num1 = display.textContent
-            console.log(num1)
-        }
-        //TODO: ilk sayi ve operator sectikten sonra ikinci sayiyi yazmaya baslayinca otomatik displayi sifirla
-        display.textContent = ''
+        calc.operator = operator.textContent
+        calc.waitingForSecNum = true
+
     })
 })
 
 equalBtn.addEventListener('click', () => {
-    if(num1 !== undefined) {
-        num2 = display.textContent
-        console.log(num2)
-        display.textContent = calculate(oper, num1, num2)
-        num1, num2, oper = undefined
+    if(calc.firstNum!==null && calc.secNum!==null && calc.operator !== null){
+        calc.displayNum = calculate(calc.operator, calc.secNum, calc.firstNum)
+        console.log('secnum:' + calc.secNum)
+        console.log('firstnum:' + calc.firstNum)
+        console.log('res:' + calc.displayNum)
+        display.textContent = calc.displayNum
     }
 })
 
 clearBtn.addEventListener('click', () => {
-    if(display.textContent === ''){
-        num1, num2, oper = undefined
-    } else {
         display.textContent = ''
-    }
+        calc.displayNum = ''
+        calc.firstNum = null
+        calc.secNum = null
+        calc.waitingForSecNum = false
+        calc.operator = null
 })
